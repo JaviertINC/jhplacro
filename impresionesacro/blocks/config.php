d018d8fb4abf0bbc9f721525ef6c80259c2a4feb
Javiert <javierthsm@gmail.com>
2019-11-28 20:16:39 -0300

    Fixed URL Home

--- impresionesacro/blocks/config.php
+++ impresionesacro/blocks/config.php
@@ -2,7 +2,7 @@
 
 $opn_token = "NtRgedkbibvTWo3cMkerX4hkdRYw7b3c";
 $url_local = "/";
-$url_home = "http://localhost/";
-$url_project = $url_home . "impresionesacro/";
+$url_home = "http://localhost";
+$url_project = $url_home . "/impresionesacro/";
 
 $project_name = "Impresiones Acro";
